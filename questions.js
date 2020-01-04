a6c060615a963fd7aa72e2cd4df835de8749890c
Kellen Presta <kpresta04@gmail.com>
2020-01-04 12:59:16 -0600

    added total of 10 questions

--- questions.js
+++ questions.js
@@ -8,6 +8,69 @@ var questions = [
     title: "The condition in an if / else statement is enclosed within ____.",
     choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
     answer: "parentheses"
+  },
+  {
+    title:
+      "What is the HTML tag under which one can write the JavaScript code?",
+    choices: ["<javascript>", "<scripted>", "<script>", "<js>"],
+    answer: "<script>"
+  },
+  {
+    title:
+      "Which of the following is the correct syntax to display 'Hello' in an alert box using JavaScript?",
+    choices: [
+      "alertbox('Hello')",
+      "msg('Hello')",
+      "msgBox('Hello')",
+      "alert('Hello')"
+    ],
+    answer: "alert('Hello')"
+  },
+  {
+    title:
+      "The external JavaScript file must contain <script> tag. True or False?",
+    choices: ["True", "False"],
+    answer: "False"
+  },
+  {
+    title: "Which of the following is not a reserved word in JavaScript?",
+    choices: ["interface", "throws", "program", "short"],
+    answer: "program"
+  },
+  {
+    title: "How is a function called in JavaScript?",
+    choices: [
+      "call aFunc()",
+      "call function aFunc()",
+      "aFunc()",
+      "function aFunc()"
+    ],
+    answer: "aFunc()"
+  },
+  {
+    title: "What is the correct syntax for adding comments in JavaScript?",
+    choices: [
+      "<!–This is a comment–&gt",
+      "//This is a comment",
+      "–This is a comment",
+      " **This is a comment**"
+    ],
+    answer: "//This is a comment"
+  },
+  {
+    title: "What is the JavaScript syntax for printing values in Console?",
+    choices: [
+      "print(5)",
+      "console.log(5)",
+      "console.print(5)",
+      "print.console(5)"
+    ],
+    answer: "console.log(5)"
+  },
+  {
+    title:
+      "What is the method in JavaScript used to remove the whitespace at the beginning and end of any string ?",
+    choices: ["strip()", "trim()", "trimmed()", "stripped()"],
+    answer: "trim()"
   }
-  ///etc.
 ];
