b0647347bc9137af6106d0f39d370f8f51998bf5
Kellen Presta <kpresta04@gmail.com>
2020-01-05 13:33:33 -0600

    added hover color to buttons

--- script.js
+++ script.js
@@ -77,6 +77,93 @@ const questions = [
 
 $(document).ready(function() {
   let disp = document.querySelector("#timer");
+  var Clock = {
+    Timer: function(duration, display) {
+      var start = Date.now(),
+        diff,
+        minutes,
+        seconds;
+      function timer() {
+        // get the number of seconds that have elapsed since
+        // startTimer() was called
+        diff = duration - (((Date.now() - start) / 1000) | 0);
+
+        // does the same job as parseInt truncates the float
+        minutes = (diff / 60) | 0;
+        seconds = diff % 60 | 0;
+        timeValue--;
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+        if (timeValue === -1) {
+          onCompletion();
+        }
+
+        if (diff <= 0) {
+          // add one second so that the count down starts at the full duration
+          // example 05:00 not 04:59
+          start = Date.now() + 1000;
+        }
+      }
+      this.Timer.timer = timer;
+    }
+  };
+  function playSound() {
+    document.querySelector("#doorbell").play();
+  }
+  function resetTimer() {
+    running = false;
+    //reset timeValue
+    timeValue = minutes * 60;
+
+    //display timeValue
+    disp.textContent = minutes + ":" + seconds + "0";
+  }
+
+  //initial variables
+  var running = false;
+  var minutes = 2.5;
+  var seconds = 0;
+  var timeValue = minutes * 60;
+
+  function startTimer() {
+    //Check if already running first
+    if (!running && timeValue >= 0) {
+      Clock.Timer(timeValue, disp);
+      // we don't want to wait a full second before the timer starts
+
+      Clock.Timer.timer();
+      interv = setInterval(Clock.Timer.timer, 1000);
+      running = true;
+    }
+  }
+  function onCompletion() {
+    playSound();
+    stopTimer();
+    $("#tomatoRow").append(
+      '<img class="tomatoImage" src="assets/images/tomato1.jpg" />'
+    );
+  }
+  function stopTimer() {
+    clearInterval(interv);
+    running = false;
+  }
+
+  //Initialize timer
+  //   resetTimer();
+  //Start button even
+  document.querySelector("#start-button").addEventListener("click", startTimer);
+  //Stop button event
+  //Reset button event
+  // this.document
+  //   .querySelector("#resetBtn")
+  //   .addEventListener("click", function() {
+  //     clearInterval(interv);
+  //     resetTimer();
+  //   });
+
   function shuffle(array) {
     //shuffles questions
     for (let i = array.length - 1; i > 0; i--) {
@@ -93,6 +180,7 @@ $(document).ready(function() {
   $("#start-button").on("click", startGame);
 
   let questionNumber = 0;
+  let score = 0;
 
   function generateQuestion(round) {
     //Clear display
@@ -128,6 +216,10 @@ $(document).ready(function() {
   }
 
   function startGame() {
+    //reset score and question number first
+    questionNumber = 0;
+    score = 0;
+
     //
 
     generateQuestion(questionNumber);
